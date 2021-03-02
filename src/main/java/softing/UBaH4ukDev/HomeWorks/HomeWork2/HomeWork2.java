b5d420065d46421547b6eba10d7eacf678a8b215
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-03-02 22:33:57 +0700

    Добавлен пакет для выполнения домашней работы №2

---
+++ src/main/java/softing/UBaH4ukDev/HomeWorks/HomeWork2/HomeWork2.java
@@ -0,0 +1,13 @@
+package softing.UBaH4ukDev.HomeWorks.HomeWork2;
+
+/****
+ Project Course2Algorithm
+ Package softing.UBaH4ukDev.HomeWorks.HomeWork2
+
+ Created by Ivan Sheynmaer (softing.UBaH4ukDev)
+
+ 2021.03.02
+ v1.0
+ */
+public class HomeWork2 {
+}
